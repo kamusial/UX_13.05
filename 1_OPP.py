9b7f90d7557acba23674a0f3ee19d7ca002bf432
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-05-13 09:43:57 +0200

    1

--- 1_OPP.py
+++ 1_OPP.py
@@ -1,10 +1,33 @@
 class Auto:
     def __init__(self, barwa, info_o_kondycji, rok_produkcji):
-        self.kolor = barwa
+        self._kolor = barwa
         self.ilosc_paliwa = 10
         self.kondycja = info_o_kondycji
-        self.spalanie_na_100 = 14
-        self.tryb_ekonomiczny = False
+        self.__spalanie_na_100 = 14
+        self.__tryb_ekonomiczny = False
         self.wiek = 2023 - rok_produkcji
+
+    def wlacz_eco(self):
+        self.__spalanie_na_100 = 8
+        self.__tryb_ekonomiczny = True
+
+    def wylacz_eco(self):
+        self.__spalanie_na_100 = 14
+        self.__tryb_ekonomiczny = False
+
+    def wyswietl_tryb(self):
+        if self.__spalanie_na_100 > 10:
+            print('spalanie jest nieco wyzsze')
+        else:
+            print('spalanie wynosi TYLKO ',self.__spalanie_na_100)
+
+moje_auto = Auto('czerwony', 3, 1965)
+# print(moje_auto.__kondycja)
+# moje_auto.__kondycja += 1
+# print(moje_auto.__kondycja)
+
+moje_auto.wlacz_eco()
+moje_auto.wyswietl_tryb()
+
 
 
