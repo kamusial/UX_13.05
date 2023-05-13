7847b6be420d21444330ea0e0fe8ca3e71744017
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-05-13 11:54:25 +0200

    1

--- 3_Kivy_label_input_gui.py
+++ 3_Kivy_label_input_gui.py
@@ -19,8 +19,8 @@ class MyGrid(GridLayout):
         self.inside.add_widget(self.imie)
 
         self.inside.add_widget(Label(text="Nazwisko: "))
-        self.Nazwisko = TextInput(multiline=False)
-        self.inside.add_widget(self.Nazwisko)
+        self.nazwisko = TextInput(multiline=False)
+        self.inside.add_widget(self.nazwisko)
 
         self.inside.add_widget(Label(text="rok_urodzenia: "))
         self.rok_urodzenia = TextInput(multiline=False)
@@ -29,8 +29,15 @@ class MyGrid(GridLayout):
         self.add_widget(self.inside)
 
         self.submit = Button(text="Submit", font_size=42)
+        self.submit.bind(on_press=self.pressed)
         self.add_widget(self.submit)
 
+    def pressed(self, instance):
+        print('wcisniety')
+        imie = self.imie.text
+        nazwisko = self.nazwisko.text
+        rok_urodz = self.rok_urodzenia.text
+        print(f'Imię: {imie}, nazwisko: {nazwisko}\nrok urodzenia: {rok_urodz}')
 
 class MyApp(App):
     def build(self):
